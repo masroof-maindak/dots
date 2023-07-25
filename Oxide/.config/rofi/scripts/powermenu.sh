e08396b5686fbf66a89ad7fae86239673af6b90d
Mujtaba Asim <62666332+MujtabaAsim@users.noreply.github.com>
2023-07-25 17:30:23 +0500

    Rofi done - pwrmnu img and logout script left

---
+++ Oxide/.config/rofi/scripts/powermenu.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+
+# Options
+shutdown=''
+reboot=''
+lock=''
+logout=''
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-mesg " Uptime: $uptime" \
+		-theme $HOME/.config/rofi/powermenu.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	if [[ $1 == '--shutdown' ]]; then
+		systemctl poweroff
+	elif [[ $1 == '--reboot' ]]; then
+		systemctl reboot
+	elif [[ $1 == '--logout' ]]; then
+		if [[ "$DESKTOP_SESSION" == 'berry' ]]; then
+		  berryc quit	
+		elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+			i3-msg exit
+		fi
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
