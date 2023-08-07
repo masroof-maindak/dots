674d5413443ae38cbfc9a0a8b95995dfe2355184
Mujtaba Asim <62666332+MujtabaAsim@users.noreply.github.com>
2023-08-07 21:48:47 +0500

    Update wezterm.lua. Everything except awm done.

--- Petrichor/.config/wezterm/wezterm.lua
+++ Petrichor/.config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.enable_tab_bar = false
 config.font = wezterm.font 'IosevkaTerm Nerd Font'
 config.font_size = 12
+config.default_cursor_style = 'SteadyUnderline'
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = "RESIZE"
 
