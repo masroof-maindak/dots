d21f02012f3429a59d8675e8543efd026fcf97f1
masroof-maindak <62666332+masroof-maindak@users.noreply.github.com>
2023-11-04 16:01:28 +0500

    swamp hyprland endeavour init.

--- Desktop/.config/fish/config.fish
+++ Desktop/.config/fish/config.fish
@@ -3,18 +3,21 @@ if status is-interactive
 
     set -Ux BROWSER firefox
     set -Ux TERMINAL foot
-    set -Ux SUDOEDITOR helix
+    set -Ux SUDOEDITOR nvim
 
     set fish_greeting
 
+    alias hypr="Hyprland"
     alias q="exit"
     alias c="clear"
-    alias h="helix"
+    alias p="pacman"
+    alias dd="dragon-drop"
+    alias v="nvim"
     alias r="ranger"
 
     alias uni="cd ~/Documents/university"
     alias cfg="cd ~/.config"
-    
+
     fish_add_path -mg $HOME/.local/bin
     fish_add_path -mg $HOME/.cargo/bin
 end
--- Desktop/.config/wezterm/wezterm.lua
+++ Desktop/.config/wezterm/wezterm.lua
@@ -1,11 +1,28 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'Oxocarbon Dark'
 config.enable_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
+
 config.font = wezterm.font('Sarasa term sc nerd')
-config.font_size = 16
+config.font_size = 15
+
+config.color_scheme = "swamp"
+config.color_schemes = {
+    ["swamp"] = {
+        foreground = "#ebe0bb",
+        background = "#242015",
+        cursor_bg = "#b8a58C",
+        cursor_border = "#d2c3a4",
+        cursor_fg = "#211d13",
+        selection_bg = "#d2c3a4" ,
+        selection_fg = "#3a3124",
+
+        ansi = {"#211d13","#a82d56","#7a7653","#db930d", "#91506c","#c1666b", "#508991","#d2c3a4"},
+        brights = {"#ebe0bb","#96294d","#736f4e","#e69a0e", "#80465f","#b05d61", "#61a0a8","#d2c3a4"},
+    },
+}
+
 config.window_padding = {
   left = 20,
   right = 20,
