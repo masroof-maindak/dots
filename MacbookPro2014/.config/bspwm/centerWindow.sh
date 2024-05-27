a81a57e04ff91b5d8a77dfec7b85e00f094e1ceb
masroof-maindak <62666332+masroof-maindak@users.noreply.github.com>
2024-05-27 18:59:23 +0500

    berry minor tweaks + eww removal

--- MacbookPro2014/.config/bspwm/centerWindow.sh
+++ MacbookPro2014/.config/bspwm/centerWindow.sh
@@ -1,8 +1,9 @@
-eval "$(bspc query -T -n | jq -r '"X=\(.client.floatingRectangle.x+(.client.floatingRectangle.width/2)-1280)\nY=\(.client.floatingRectangle.y+(.client.floatingRectangle.height/2)-800)"')"
-# Remove decimal part if it's .5
+eval "$(bspc query -T -n | jq -r '"
+    X=\(.client.floatingRectangle.x+(.client.floatingRectangle.width/2)-1280)
+    Y=\(.client.floatingRectangle.y+(.client.floatingRectangle.height/2)-800)
+"')"
+
+# Remove decimal part
 X=$(echo "$X" | awk '{ printf("%.0f\n", $1) }')
 Y=$(echo "$Y" | awk '{ printf("%.0f\n", $1) }')
-dx=$((X * -1))
-dy=$((Y * -1))
-bspc node -v $dx $dy
-
+bspc node -v $((X * -1)) $((Y * -1))
